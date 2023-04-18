38d781039fcd043fd1b4f810eb721d18fcf27e31
Suhho Lee <suhholeemichael@gmail.com>
2023-04-19 02:47:34 +0900

    first try of making the team selection page

--- client/src/components/main/TeamSelection.js
+++ client/src/components/main/TeamSelection.js
@@ -13,14 +13,95 @@ const TeamSelection = ({ getUserInfo }) => {
   // ! Variables
   const { userId } = useParams()
   const navigate = useNavigate()
-  const positions = ['GK', 'DF', 'MF', 'FW']
-  const formation = [
-    ['GK'],
-    ['DF', 'DF', 'DF', 'DF'],
-    ['MF', 'MF', 'MF', 'MF'],
-    ['FW', 'FW']
-  ]
-
+  const [selectedGK, setSelectedGK] = useState({
+    name: 'GK',
+    position: 'GK',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedLB, setSelectedLB] = useState({
+    name: 'DF',
+    position: 'DF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedLCB, setSelectedLCB] = useState({
+    name: 'DF',
+    position: 'DF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedRCB, setSelectedRCB] = useState({
+    name: 'DF',
+    position: 'DF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedRB, setSelectedRB] = useState({
+    name: 'DF',
+    position: 'DF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedLM, setSelectedLM] = useState({
+    name: 'MF',
+    position: 'MF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedLCM, setSelectedLCM] = useState({
+    name: 'MF',
+    position: 'MF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedRCM, setSelectedRCM] = useState({
+    name: 'MF',
+    position: 'MF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedRM, setSelectedRM] = useState({
+    name: 'MF',
+    position: 'MF',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedLF, setSelectedLF] = useState({
+    name: 'FW',
+    position: 'FW',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  const [selectedRF, setSelectedRF] = useState({
+    name: 'FW',
+    position: 'FW',
+    team: {
+      logo: '',
+      next_match: '',
+    },
+  })
+  
   // ! State
   const [info, setInfo] = useState([])
   const [players, setPlayers] = useState([])
@@ -72,11 +153,48 @@ const TeamSelection = ({ getUserInfo }) => {
       console.log(data)
       getUserInfo()
       setShowModal(false)
+      if (player.position === 'GK') {
+        setSelectedGK(player)
+      }
+      if (player.position === 'DF' && selectedRB.name === 'DF' && selectedRCB.name === 'DF' && selectedLCB.name === 'DF' && selectedLB.name === 'DF') {
+        setSelectedRB(player)
+      } else if (player.position === 'DF' && selectedRCB.name === 'DF' && selectedLCB.name === 'DF' && selectedLB.name === 'DF') {
+        setSelectedRCB(player)
+      } else if (player.position === 'DF' && selectedLCB.name === 'DF' && selectedLB.name === 'DF') {
+        setSelectedLCB(player)
+      } else if (player.position === 'DF' && selectedLB.name === 'DF') {
+        setSelectedLB(player)
+      }
+      if (player.position === 'MF' && selectedRM.name === 'MF' && selectedRCM.name === 'MF' && selectedLCM.name === 'MF' && selectedLM.name === 'MF') {
+        setSelectedRM(player)
+      } else if (player.position === 'MF' && selectedRCM.name === 'MF' && selectedLCM.name === 'MF' && selectedLM.name === 'MF') {
+        setSelectedRCM(player)
+      } else if (player.position === 'MF' && selectedLCM.name === 'MF' && selectedLM.name === 'MF') {
+        setSelectedLCM(player)
+      } else if (player.position === 'MF' && selectedLM.name === 'MF') {
+        setSelectedLM(player)
+      }
+      if (player.position === 'FW' && selectedRF.name === 'FW' && selectedLF.name === 'FW') {
+        setSelectedRF(player)
+      } else if (player.position === 'FW' && selectedLF.name === 'FW') {
+        setSelectedLF(player)
+      }
     } catch (err) {
       console.log(err.response)
       setInfoError(err.response.request.responseText)
     }
   }
+
+  const handlePositionClick = (position) => {
+    setSelectedPosition(position)
+    setShowModal(true)
+    getUserInfo()
+  }
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
 
   return (
     <main className='team-selection'>
@@ -85,6 +203,49 @@ const TeamSelection = ({ getUserInfo }) => {
         {info ?
           <Container className='selection-container'>
             <h4>Budget: {info.budget}m</h4>
+            <div className='formation'>
+              <div className='gk'>
+                <div className='position-button' onClick={() => handlePositionClick(selectedGK.position)}>
+                  <img className='logo' src={selectedGK.team.logo}></img>{selectedGK.name}
+                </div>
+              </div>
+              <div className='df'>
+                <div className='position-button' onClick={() => handlePositionClick(selectedRB.position)}>
+                  <img className='logo' src={selectedRB.team.logo}></img>{selectedRB.name}
+                </div>
+                <div className='position-button' onClick={() => handlePositionClick(selectedRCB.position)}>
+                  <img className='logo' src={selectedRCB.team.logo}></img>{selectedRCB.name}
+                </div>
+                <div className='position-button' onClick={() => handlePositionClick(selectedLCB.position)}>
+                  <img className='logo' src={selectedLCB.team.logo}></img>{selectedLCB.name}
+                </div>
+                <div className='position-button' onClick={() => handlePositionClick(selectedLB.position)}>
+                  <img className='logo' src={selectedLB.team.logo}></img>{selectedLB.name}
+                </div>
+              </div>
+              <div className='mf'>
+                <div className='position-button' onClick={() => handlePositionClick(selectedRM.position)}>
+                  <img className='logo' src={selectedRM.team.logo}></img>{selectedRM.name}
+                </div>
+                <div className='position-button' onClick={() => handlePositionClick(selectedRCM.position)}>
+                  <img className='logo' src={selectedRCM.team.logo}></img>{selectedRCM.name}
+                </div>
+                <div className='position-button' onClick={() => handlePositionClick(selectedLCM.position)}>
+                  <img className='logo' src={selectedLCM.team.logo}></img>{selectedLCM.name}
+                </div>
+                <div className='position-button' onClick={() => handlePositionClick(selectedLM.position)}>
+                  <img className='logo' src={selectedLM.team.logo}></img>{selectedLM.name}
+                </div>
+              </div>
+              <div className='fw'>
+                <div className='position-button' onClick={() => handlePositionClick(selectedRF.position)}>
+                  <img className='logo' src={selectedRF.team.logo}></img>{selectedRF.name}
+                </div>
+                <div className='position-button' onClick={() => handlePositionClick(selectedLF.position)}>
+                  <img className='logo' src={selectedLF.team.logo}></img>{selectedLF.name}
+                </div>
+              </div>
+            </div>
           </Container>
           :
           <>
@@ -96,33 +257,38 @@ const TeamSelection = ({ getUserInfo }) => {
           </>
         }
         {players ?
-          <Container className='players-container'>
-            <Table hover size="sm" className='player-table'>
-              {/* Headers */}
-              <thead>
-                <tr className='text-center'>
-                  <th>Player</th>
-                  <th>Position</th>
-                  <th>Price</th>
-                  <th>Total Points</th>
-                </tr>
-              </thead>
-              {/* Body */}
-              {players.sort((a, b) => a.price < b.price ? 1 : -1).map(player => {
-                const { id, name, position, price, total_points, team: { logo: logo } } = player
-                return (
-                  <tbody key={id}>
-                    <tr className='text-center' value={id} onClick={() => selectPlayer(player)}>
-                      <td className='text-start name'><img className='logo' src={logo}></img>{name}</td>
-                      <td>{position}</td>
-                      <td>{price}</td>
-                      <td>{total_points}</td>
-                    </tr>
-                  </tbody>
-                )
-              })}
-            </Table>
-          </Container>
+          <Modal show={showModal} onHide={handleClose} className='players-container'>
+            <Modal.Header closeButton>
+              <Modal.Title>{selectedPosition}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Table hover className='player-table'>
+                {/* Headers */}
+                <thead>
+                  <tr className='text-center'>
+                    <th>Player</th>
+                    <th>Position</th>
+                    <th>Price</th>
+                    <th>Total Points</th>
+                  </tr>
+                </thead>
+                {/* Body */}
+                {players.sort((a, b) => a.price < b.price ? 1 : -1).filter((player) => player.position === selectedPosition).map(player => {
+                  const { id, name, position, price, total_points, team: { logo: logo } } = player
+                  return (
+                    <tbody key={id}>
+                      <tr className='text-center' value={id} onClick={() => selectPlayer(player)}>
+                        <td className='text-start name'><img className='logo' src={logo}></img>{name}</td>
+                        <td>{position}</td>
+                        <td>{price}</td>
+                        <td>{total_points}</td>
+                      </tr>
+                    </tbody>
+                  )
+                })}
+              </Table>
+            </Modal.Body>
+          </Modal>
           :
           <>
             {playersError ?
