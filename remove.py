4a392c2fd4b313939e48e817e230bac501a6c056
Suhho Lee <suhholeemichael@gmail.com>
2023-04-29 16:08:45 +0900

    info object gw points reset added to remove.py

--- remove.py
+++ remove.py
@@ -5,10 +5,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
 
 from players.models import Player
+from info.models import Info
 
 def remove_points():
 
     Player.objects.update(gw_points=0)
+    Info.objects.update(gw_points=0)
 
 if __name__ == '__main__':
     remove_points()
